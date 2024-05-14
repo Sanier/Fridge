8c6a9a7d24919a140cbd6d7edd7924fa99bfb3ff
Sanier <sanier20@mail.ru>
2024-05-14 10:00:17 +0300

    Небольшие изменения

--- Fridge/Program.cs
+++ Fridge/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(opt =>
 {
-    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "Readbooru", Version = "v1" });
+    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "Fridge", Version = "v1" });
     opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
         In = ParameterLocation.Header,
